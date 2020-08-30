74b6bf01592fd9a494696262dff9f84ac2781497
Chase Teichmann <cteichmann@returnlogic.com>
2020-08-30 00:54:09 -0400

    break:

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 //Date Calculator for years experience. I manually enter start date and it calculates years and months of experience
 // from present day so I don't have to continually update
+
+function myFunction() {
+    var copyText = document.getElementById("myInput");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    document.execCommand("copy");
+
+    var tooltip = document.getElementById("myTooltip");
+    tooltip.innerHTML = "Copied: " + copyText.value;
+}
+
+function outFunc() {
+    var tooltip = document.getElementById("myTooltip");
+    tooltip.innerHTML = "Copy to clipboard";
+}
